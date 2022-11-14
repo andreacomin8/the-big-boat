45193e5624e66823d8dd0206cb94be85cf51dc0b
Maxsteal91 <116298429+Maxsteal91@users.noreply.github.com>
2022-11-14 22:58:28 +0100

    exp_date

--- app_refactor.py
+++ app_refactor.py
@@ -2,15 +2,22 @@
 import os, sys
 #os.chdir(sys._MEIPASS)
 
-
-# todo: import classes from outer files
 import page_launcher
+from tkinter import messagebox as mb
+from datetime import datetime
 
+today = datetime.now()
+exp_date = datetime(2022, 12, 15)
 
 def start_application():
     page_launcher.launch_landing_page()
 
-start_application()
+if today > exp_date:
+    start_application()
+else:
+    mb.showwarning("Attenzione Licenza Scaduta")
+
+
 
 
 
